f5a866d059c9293f6db46ed37d822d7a81af942a
Andre <andressluis33@gmail.com>
2026-03-28 17:52:28 -0300

    falta fazer querys, joins, triggers, etc

---
+++ ecommerce_portfolio.sql
@@ -0,0 +1,56 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.8.1-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: ecommerce_portfolio
+-- ------------------------------------------------------
+-- Server version	11.8.1-MariaDB-4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `usuarios` VALUES
+(1,'André','andressluis@gmail.com');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2026-03-24 20:27:59
---
+++ sql/01_create_tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE "usuarios" (
+    "id" SERIAL PRIMARY KEY,
+    "nome" VARCHAR(255) NOT NULL,
+    "endereco" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "senha" VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE "produtos" (
+    "id" SERIAL PRIMARY KEY,
+    "nome" VARCHAR(255) NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "preco" DECIMAL(10, 2) NOT NULL,
+    "quantidade_estoque" INT NOT NULL
+);
+
+CREATE TABLE "compras" (
+    "id" SERIAL PRIMARY KEY,
+    "id_produto" INT NOT NULL FOREIGN KEY REFERENCES "produtos" ("id"),
+    "usuario_id" INT NOT NULL FOREIGN KEY REFERENCES "usuarios" ("id"),
+    "data_pedido" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id_carrinho" INT NOT NULL FOREIGN KEY REFERENCES "carrinhos" ("id") 
+
+);
+
+CREATE TABLE "carrinhos" (
+    "id" SERIAL PRIMARY KEY,
+    "usuario_id" INT NOT NULL FOREIGN KEY REFERENCES "usuarios" ("id"),
+    "data_criacao" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
